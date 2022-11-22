28c22e6c047c10a1f350fe25eba7f27e6ef20dc6
Adazajac <89804767+Adazajac@users.noreply.github.com>
2022-11-22 14:31:10 +0100

    Repair xml file

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use SimpleXMLElement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DomCrawler\Crawler;
 
 class HomeController extends AbstractController
 {
+
     /**
      * @Route("/", name="home")
      */
@@ -32,11 +35,32 @@ class HomeController extends AbstractController
     /**
      * @Route("/show/{id}", name="show")
      */
-    public function show(Article $article, Request $request)
+    public function show(Article $article)
     {
-        
+
+        $xmlPath = $article->getFile();
+        $xmlLast = substr($xmlPath, -3);
+        if ($xmlLast == 'xml') {
+
+
+            $xmlstr = file_get_contents('C:\Test\Symfony\WebProject\public\files' . '\\' . $xmlPath);
+            $xml = simplexml_load_string($xmlstr);
+
+            $json = json_encode($xml);
+            $array = json_decode($json, TRUE);
+            array_walk_recursive($array, function ($item, $key) use (&$results) {
+                $results[$key] = $item;
+            });
+        } else {
+            $results = '';
+
+        }
+
+
+//        dd($results);
         return $this->render('home/show.html.twig', [
-            'article' => $article
+            'article' => $article,
+            'results' => $results
         ]);
     }
 
